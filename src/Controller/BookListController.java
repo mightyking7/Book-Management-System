26966412c910353f73feab512354eb03cf5f6fcc
Hercules Hjalmarsson <ert283@my.utsa.edu>
2018-09-09 11:20:07 -0500

    archiving

--- src/Controller/BookListController.java
+++ src/Controller/BookListController.java
@@ -5,8 +5,6 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 import java.time.Month;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
