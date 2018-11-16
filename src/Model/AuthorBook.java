8a618016515ab5d6ae9cb8c73efbfd61138483b7
Hercules Hjalmarsson <ert283@my.utsa.edu>
2018-11-16 09:56:19 -0600

    Model update

---
+++ src/Model/AuthorBook.java
@@ -0,0 +1,50 @@
+package Model;
+
+public class AuthorBook {
+
+	private Author author;
+	private Book book;
+	private int royalty;
+	private boolean newRecord = true;
+	
+	public AuthorBook()
+	{
+		author = null;
+		book = null;
+		royalty = 0;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public int getRoyalty() {
+		return royalty;
+	}
+
+	public void setRoyalty(int royalty) {
+		this.royalty = royalty;
+	}
+
+	public boolean isNewRecord() {
+		return newRecord;
+	}
+
+	public void setNewRecord(boolean newRecord) {
+		this.newRecord = newRecord;
+	}
+	
+	
+}
